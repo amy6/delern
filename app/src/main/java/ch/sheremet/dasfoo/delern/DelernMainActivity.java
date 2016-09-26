29c83049fe98e79015562da02963ea10a9de0733
Katarina Sheremet <kate.romanowskaya@gmail.com>
2016-09-26 20:19:08 +0200

    Open new Fragment on item click

--- app/src/main/java/ch/sheremet/dasfoo/delern/DelernMainActivity.java
+++ app/src/main/java/ch/sheremet/dasfoo/delern/DelernMainActivity.java
@@ -1,15 +1,19 @@
 package ch.sheremet.dasfoo.delern;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class DelernMainActivity extends AppCompatActivity {
+import ch.sheremet.dasfoo.delern.card.CardFragment;
+
+public class DelernMainActivity extends AppCompatActivity
+        implements CardFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +30,12 @@ public class DelernMainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        DelernMainActivityFragment listFragment = new DelernMainActivityFragment();
+        // Add the fragment to the 'fragment_container' FrameLayout
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragment_container, listFragment).commit();
+
     }
 
     @Override
@@ -48,5 +58,10 @@ public class DelernMainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        
     }
 }
--- app/src/main/java/ch/sheremet/dasfoo/delern/DelernMainActivityFragment.java
+++ app/src/main/java/ch/sheremet/dasfoo/delern/DelernMainActivityFragment.java
@@ -1,8 +1,10 @@
 package ch.sheremet.dasfoo.delern;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -13,12 +15,14 @@ import android.widget.Toast;
 import com.yqritc.recyclerviewflexibledivider.HorizontalDividerItemDecoration;
 
 import ch.sheremet.dasfoo.delern.adapters.ListAdapter;
+import ch.sheremet.dasfoo.delern.card.CardFragment;
 import ch.sheremet.dasfoo.delern.listeners.RecyclerItemClickListener;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class DelernMainActivityFragment extends Fragment {
+public class DelernMainActivityFragment extends Fragment
+        implements CardFragment.OnFragmentInteractionListener {
 
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
@@ -31,6 +35,7 @@ public class DelernMainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_delern_main, container, false);
+
         //TODO: move init to onActivityCreated
         mRecyclerView = (RecyclerView) rootView.findViewById(R.id.list_recycler_view);
         mRecyclerView.addItemDecoration(new HorizontalDividerItemDecoration.Builder(getActivity())
@@ -38,8 +43,25 @@ public class DelernMainActivityFragment extends Fragment {
 
         mRecyclerView.addOnItemTouchListener(
                 new RecyclerItemClickListener(getContext(), new RecyclerItemClickListener.OnItemClickListener() {
-                    @Override public void onItemClick(View view, int position) {
-                        Toast.makeText(getContext(),"Position = "+ position, Toast.LENGTH_SHORT).show();
+                    @Override
+                    public void onItemClick(View view, int position) {
+                        Toast.makeText(getContext(), "Position = " + position, Toast.LENGTH_SHORT).show();
+                        // Create fragment and give it an argument specifying the article it should show
+                        CardFragment newFragment = new CardFragment();
+                        //      Bundle args = new Bundle();
+                        //     args.putInt(ArticleFragment.ARG_POSITION, position);
+                        //     newFragment.setArguments(args);
+
+                        FragmentTransaction transaction = getActivity().getSupportFragmentManager()
+                                .beginTransaction();
+
+// Replace whatever is in the fragment_container view with this fragment,
+// and add the transaction to the back stack so the user can navigate back
+                        transaction.replace(R.id.fragment_container, newFragment);
+                        transaction.addToBackStack(null);
+
+// Commit the transaction
+                        transaction.commit();
                     }
                 })
         );
@@ -62,5 +84,10 @@ public class DelernMainActivityFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
