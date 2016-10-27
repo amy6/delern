632239016ebdbf3da936f355d7fe5f2c237eb7a7
Katarina Sheremet <kate.romanowskaya@gmail.com>
2016-10-27 12:09:50 +0200

    Send cards as parameter to ShowCardsFragment

--- app/src/main/java/org/dasfoo/delern/models/Card.java
+++ app/src/main/java/org/dasfoo/delern/models/Card.java
@@ -1,10 +1,13 @@
 package org.dasfoo.delern.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by katarina on 10/4/16.
  */
 
-public class Card {
+public class Card implements Parcelable {
     private String uid;
     private String backside;
     private String frontside;
@@ -23,6 +26,24 @@ public class Card {
         this.backside = backSide;
         this.frontside = frontSide;
     }
+
+    protected Card(Parcel in) {
+        uid = in.readString();
+        backside = in.readString();
+        frontside = in.readString();
+    }
+
+    public static final Creator<Card> CREATOR = new Creator<Card>() {
+        @Override
+        public Card createFromParcel(Parcel in) {
+            return new Card(in);
+        }
+
+        @Override
+        public Card[] newArray(int size) {
+            return new Card[size];
+        }
+    };
 
     public String getUid() {
         return uid;
@@ -46,5 +67,44 @@ public class Card {
 
     public void setFrontSide(String frontSide) {
         this.frontside = frontSide;
+    }
+
+    /**
+     * Describe the kinds of special objects contained in this Parcelable
+     * instance's marshaled representation. For example, if the object will
+     * include a file descriptor in the output of {@link #writeToParcel(Parcel, int)},
+     * the return value of this method must include the
+     * {@link #CONTENTS_FILE_DESCRIPTOR} bit.
+     *
+     * @return a bitmask indicating the set of special object types marshaled
+     * by this Parcelable object instance.
+     * @see #CONTENTS_FILE_DESCRIPTOR
+     */
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    /**
+     * Flatten this object in to a Parcel.
+     *
+     * @param dest  The Parcel in which the object should be written.
+     * @param flags Additional flags about how the object should be written.
+     *              May be 0 or {@link #PARCELABLE_WRITE_RETURN_VALUE}.
+     */
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.uid);
+        dest.writeString(this.backside);
+        dest.writeString(this.frontside);
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "uid='" + uid + '\'' +
+                ", backside='" + backside + '\'' +
+                ", frontside='" + frontside + '\'' +
+                '}';
     }
 }
