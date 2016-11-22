cb1d552ddfd9cc50f142b6dc1d1bd32f7fff6a17
Katarina Sheremet <kate.romanowskaya@gmail.com>
2016-11-22 16:58:59 +0100

    Remove unused imports

--- app/src/main/java/org/dasfoo/delern/card/EditCardListActivity.java
+++ app/src/main/java/org/dasfoo/delern/card/EditCardListActivity.java
@@ -13,7 +13,6 @@ import org.dasfoo.delern.BaseActivity;
 import org.dasfoo.delern.R;
 import org.dasfoo.delern.adapters.CardRecyclerViewAdapter;
 import org.dasfoo.delern.models.Card;
-import org.dasfoo.delern.util.LogUtil;
 import org.dasfoo.delern.viewholders.CardViewHolder;
 
 public class EditCardListActivity extends BaseActivity {
