91114df9de86002a740a51c4bf3ae5dc271d4238
Katarina Sheremet <kate.romanowskaya@gmail.com>
2017-01-11 22:48:56 +0100

    Enable adding reversed card

--- app/src/main/java/org/dasfoo/delern/card/AddEditCardActivity.java
+++ app/src/main/java/org/dasfoo/delern/card/AddEditCardActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.Toast;
 
 import org.dasfoo.delern.R;
@@ -37,6 +38,7 @@ public class AddEditCardActivity extends AppCompatActivity implements View.OnCli
     private TextInputEditText mFrontSideInputText;
     private TextInputEditText mBackSideInputText;
     private Card mCard;
+    private CheckBox mAddReversedCardCheckbox;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -51,6 +53,7 @@ public class AddEditCardActivity extends AppCompatActivity implements View.OnCli
 
         mFrontSideInputText = (TextInputEditText) findViewById(R.id.front_side_text);
         mBackSideInputText = (TextInputEditText) findViewById(R.id.back_side_text);
+        mAddReversedCardCheckbox = (CheckBox) findViewById(R.id.add_reversed_card_checkbox);
         Button mAddCardToDbButton = (Button) findViewById(R.id.add_card_to_db);
         if (mCard != null) {
             mAddCardToDbButton.setText(R.string.save);
@@ -69,14 +72,18 @@ public class AddEditCardActivity extends AppCompatActivity implements View.OnCli
     public void onClick(final View v) {
         if (v.getId() == R.id.add_card_to_db) {
             if (mCard == null) {
-                Card newCard = new Card();
-                newCard.setFront(mFrontSideInputText.getText().toString());
-                newCard.setBack(mBackSideInputText.getText().toString());
-                newCard.setLevel(Level.L0.name());
-                newCard.setRepeatAt(System.currentTimeMillis());
-                Card.createNewCard(newCard, mDeckId);
+                String frontCardSide = mFrontSideInputText.getText().toString();
+                String backCardSide = mBackSideInputText.getText().toString();
+                addNewCard(frontCardSide, backCardSide);
+                if (mAddReversedCardCheckbox.isChecked()) {
+                    addNewCard(backCardSide, frontCardSide);
+                    Toast.makeText(this, R.string.add_extra_reversed_card_message,
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, R.string.added_card_user_message,
+                            Toast.LENGTH_SHORT).show();
+                }
                 cleanTextFields();
-                Toast.makeText(this, R.string.added_card_user_message, Toast.LENGTH_SHORT).show();
             } else {
                 mCard.setFront(mFrontSideInputText.getText().toString());
                 mCard.setBack(mBackSideInputText.getText().toString());
@@ -100,5 +107,14 @@ public class AddEditCardActivity extends AppCompatActivity implements View.OnCli
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+    }
+
+    private void addNewCard(final String frontSide, final String backSide) {
+        Card newCard = new Card();
+        newCard.setFront(frontSide);
+        newCard.setBack(backSide);
+        newCard.setLevel(Level.L0.name());
+        newCard.setRepeatAt(System.currentTimeMillis());
+        Card.createNewCard(newCard, mDeckId);
     }
 }
