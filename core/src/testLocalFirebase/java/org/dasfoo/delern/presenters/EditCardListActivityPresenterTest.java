61f052355f34f9eca876b8d1eeda1d4074c23e3b
Katarina Sheremet <kate.romanowskaya@gmail.com>
2017-08-17 10:04:47 +0200

    Test EditCardListActivityPresenter #186
    relnote: none

---
+++ core/src/testLocalFirebase/java/org/dasfoo/delern/presenters/EditCardListActivityPresenterTest.java
@@ -0,0 +1,43 @@
+package org.dasfoo.delern.presenters;
+
+import org.dasfoo.delern.models.Card;
+import org.dasfoo.delern.models.Deck;
+import org.dasfoo.delern.models.User;
+import org.dasfoo.delern.test.FirebaseServerUnitTest;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by katarina on 8/17/17.
+ */
+
+public class EditCardListActivityPresenterTest extends FirebaseServerUnitTest {
+
+    private EditCardListActivityPresenter mPresenter;
+    private Deck mDeck;
+
+    @Before
+    public void setParam() throws Exception {
+        mPresenter = new EditCardListActivityPresenter();
+        User user = signIn();
+        user.save().blockingAwait();
+        mDeck = new Deck(user);
+        mDeck.setAccepted(true);
+        mDeck.setName("test");
+        mDeck.create().blockingAwait();
+        mPresenter.onCreate(mDeck);
+
+    }
+
+    @Test
+    public void getDeckTest() {
+        assertEquals(mDeck, mPresenter.getDeck());
+    }
+
+    @Test
+    public void getQueryTest() {
+        assertEquals(mPresenter.getQuery(), mDeck.getChildReference(Card.class));
+    }
+}
