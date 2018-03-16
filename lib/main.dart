6189d18285fb9704325e9b1fbd5060a3ce9c5d79
Artem Sheremet <dot.doom@gmail.com>
2018-03-16 21:24:17 +0100

    Add Sentry error reporting from Flutter
    relnote: none

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,50 @@
+import 'dart:async';
+import 'dart:isolate';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:sentry/sentry.dart';
 
-void main() => runApp(new MyApp());
+// Error reporting inspired (or rather, copied 1:1)
+// by https://github.com/yjbanov/crashy/
+final SentryClient _sentry = new SentryClient(
+    dsn: "https://36d72a65344d439d86ee65d623d050ce:" +
+        "038b2b2aa94f474db45ce1c4676b845e@sentry.io/305345");
+
+Future _reportError(dynamic error, dynamic stackTrace) async {
+  print('Caught error: $error');
+  print('Reporting to Sentry.io...');
+  final SentryResponse response = await _sentry.captureException(
+    exception: error,
+    stackTrace: stackTrace,
+  );
+  if (response.isSuccessful) {
+    print('Success! Event ID: ${response.eventId}');
+  } else {
+    print('Failed to report to Sentry.io: ${response.error}');
+  }
+}
+
+void main() {
+  FlutterError.onError = (FlutterErrorDetails details) async {
+    print('FlutterError.onError caught an error');
+    await _reportError(details.exception, details.stack);
+  };
+  Isolate.current.addErrorListener(new RawReceivePort((dynamic pair) async {
+    print('Isolate.current.addErrorListener caught an error');
+    await _reportError(
+      (pair as List<String>).first,
+      (pair as List<String>).last,
+    );
+  }).sendPort);
+
+  runZoned<Future>(() async {
+    runApp(new MyApp());
+  }, onError: (error, stackTrace) async {
+    print('Zone caught an error');
+    await _reportError(error, stackTrace);
+  });
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
