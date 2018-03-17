d344ed478c46d1baf9da77c9c3d666a949940a1e
Artem Sheremet <dot.doom@gmail.com>
2018-03-17 16:44:57 +0100

    Remove Android application
    Adding both Flutter Firebase and Android Firebase throws app into
    MultiDex
    
    relnote: none

--- android/app/src/main/java/org/dasfoo/delern/MainActivity.java
+++ android/app/src/main/java/org/dasfoo/delern/MainActivity.java
@@ -3,22 +3,12 @@ package org.dasfoo.delern;
 import android.os.Bundle;
 
 import io.flutter.app.FlutterActivity;
-import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        GeneratedPluginRegistrant.registerWith(this);
-        new MethodChannel(getFlutterView(), "org.dasfoo.delern/android").setMethodCallHandler(
-                (call, result) -> {
-                    if (call.method.equals("Main")) {
-                        SplashScreenActivity.startActivity(getApplicationContext());
-                        result.success(null);
-                    } else {
-                        result.notImplemented();
-                    }
-                });
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
 }
