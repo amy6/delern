4ce40b57052c600e2d77936daa2b4b2b526f0a7f
Katarina Sheremet <kate.romanowskaya@gmail.com>
2018-03-17 14:12:15 +0100

    Add toolbar in SupportDevFragment
    relnote: none

--- app/src/main/java/org/dasfoo/delern/billing/SupportDevFragment.java
+++ app/src/main/java/org/dasfoo/delern/billing/SupportDevFragment.java
@@ -18,12 +18,16 @@
 
 package org.dasfoo.delern.billing;
 
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -61,6 +65,18 @@ public class SupportDevFragment extends DialogFragment {
         setStyle(DialogFragment.STYLE_NORMAL, R.style.AppTheme);
     }
 
+    private void configureToolbar(final View root) {
+        Toolbar toolbar = root.findViewById(R.id.toolbar);
+
+        final Drawable upArrow = ContextCompat.getDrawable(root.getContext(),
+                R.drawable.ic_arrow_back_black_24dp);
+        upArrow.setColorFilter(ContextCompat.getColor(root.getContext(),
+                android.R.color.white), PorterDuff.Mode.SRC_ATOP);
+        toolbar.setNavigationIcon(upArrow);
+        toolbar.setNavigationOnClickListener(v -> dismiss());
+        toolbar.setTitle(R.string.nav_support_development);
+    }
+
     /**
      * {@inheritDoc}
      */
@@ -73,6 +89,7 @@ public class SupportDevFragment extends DialogFragment {
         mErrorTextView = root.findViewById(R.id.error_textview);
         mRecyclerView = root.findViewById(R.id.list);
         mLoadingView = root.findViewById(R.id.progress_bar);
+        configureToolbar(root);
         setWaitScreen(true);
         onManagerReady((BillingProvider) getActivity());
         return root;
