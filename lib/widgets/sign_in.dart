19b94c54c62413cd611dd3e95443b2ed59aafd60
Katarina Sheremet <kate.romanowskaya@gmail.com>
2018-04-13 13:01:01 +0200

    Implement Sign In screen
    relnote: none

--- lib/widgets/sign_in.dart
+++ lib/widgets/sign_in.dart
@@ -1,6 +1,5 @@
-import 'package:flutter/material.dart';
-
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 
 import '../remote/sign_in.dart';
 
@@ -13,16 +12,56 @@ class SignInWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return new Column(
+    return new Stack(
       children: <Widget>[
-        new MaterialButton(
+        new Column(
+          children: <Widget>[
+            new Expanded(
+              child: new Container(
+                child: new Center(
+                  child: new Image.asset(
+                    'images/delern.png',
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        new Container(
+          padding: const EdgeInsets.only(bottom: 50.0, left: 15.0, right: 15.0),
           child: new Row(
             children: <Widget>[
-              new Icon(Icons.supervisor_account),
-              new Text('Sign in with Google'),
+              new Expanded(
+                child: new Align(
+                    alignment: Alignment.bottomCenter,
+                    child: new RaisedButton(
+                        color: Colors.white,
+                        onPressed: () async =>
+                            onSignedIn(await signInGoogleUser()),
+                        child: new Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: <Widget>[
+                            new Container(
+                              padding: const EdgeInsets.all(10.0),
+                              child: new Image.asset(
+                                'images/google_sign_in.png',
+                                height: 35.0,
+                                width: 35.0,
+                              ),
+                            ),
+                            new Container(
+                                padding: const EdgeInsets.only(left: 10.0),
+                                child: new Text(
+                                  'Sign In with Google',
+                                  style: new TextStyle(
+                                    fontSize: 18.0,
+                                  ),
+                                )),
+                          ],
+                        ))),
+              ),
             ],
           ),
-          onPressed: () async => onSignedIn(await signInGoogleUser()),
         )
       ],
     );
