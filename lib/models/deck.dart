29a6515ed40153a7f08551cbe69e5cfc8b3519d8
Artem Sheremet <dot.doom@gmail.com>
2018-04-30 08:09:30 +0200

    Add model methods for Deck
    relnote: none

--- lib/models/deck.dart
+++ lib/models/deck.dart
@@ -2,17 +2,45 @@ import 'dart:async';
 import 'dart:core';
 
 import 'package:firebase_database/firebase_database.dart';
+import 'package:meta/meta.dart';
 
 import 'keyed_list.dart';
 import 'observable_list.dart';
 
+enum DeckType { basic, german, swiss }
+
 class Deck implements KeyedListItem {
-  final String key;
-  final String name;
   final String uid;
+  String key;
+
+  String name;
+  bool markdown;
+  DeckType type;
+  bool accepted;
+  DateTime lastSyncAt;
+  String category;
+
+  Deck(
+    this.uid, {
+    @required this.name,
+    this.markdown: false,
+    this.type: DeckType.basic,
+    this.accepted: true,
+    this.lastSyncAt,
+    this.category,
+  }) {
+    lastSyncAt ??= new DateTime.fromMillisecondsSinceEpoch(0);
+  }
 
   Deck.fromSnapshot(this.key, dynamic snapshotValue, this.uid)
-      : name = snapshotValue['name'];
+      : name = snapshotValue['name'],
+        markdown = snapshotValue['markdown'] ?? false,
+        type = _stringToDeckType(snapshotValue['deckType']),
+        accepted = snapshotValue['accepted'] ?? false,
+        lastSyncAt = new DateTime.fromMillisecondsSinceEpoch(
+            snapshotValue['lastSyncAt'],
+            isUtc: true),
+        category = snapshotValue['category'];
 
   static Stream<KeyedListEvent<Deck>> getDecks(String uid) async* {
     yield new KeyedListEvent(
@@ -37,6 +65,29 @@ class Deck implements KeyedListItem {
         (snapshot) => new Deck.fromSnapshot(snapshot.key, snapshot.value, uid));
   }
 
+  Future<void> updateExisting() => FirebaseDatabase.instance
+      .reference()
+      .child('decks')
+      .child(uid)
+      .child(key)
+      .set(_toMap());
+
+  Future<void> saveAsNew() {
+    key = FirebaseDatabase.instance
+        .reference()
+        .child('decks')
+        .child(uid)
+        .push()
+        .key;
+
+    return updateExisting().then((_) => FirebaseDatabase.instance
+        .reference()
+        .child('deck_access')
+        .child(key)
+        .child(uid)
+        .set({'access': 'owner'}));
+  }
+
   Stream<String> getAccess() => FirebaseDatabase.instance
       .reference()
       .child('deck_access')
@@ -46,6 +97,11 @@ class Deck implements KeyedListItem {
       .onValue
       .map((evt) => evt.snapshot.value as String);
 
+  static _stringToDeckType(String value) => DeckType.values
+      .firstWhere((deckType) => deckType.toString().toLowerCase() == value);
+
+  static _deckTypeToString(DeckType value) => value.toString().split('.')[-1];
+
   Stream<int> getNumberOfCardsToLearn([int limit = 201]) =>
       FirebaseDatabase.instance
           .reference()
@@ -53,8 +109,17 @@ class Deck implements KeyedListItem {
           .child(uid)
           .child(key)
           .orderByChild('repeatAt')
-          .endAt(new DateTime.now().millisecondsSinceEpoch)
+          .endAt(new DateTime.now().toUtc().millisecondsSinceEpoch)
           .limitToFirst(limit)
           .onValue
           .map((evt) => (evt.snapshot.value as Map)?.length ?? 0);
+
+  Map<String, dynamic> _toMap() => {
+        'name': name,
+        'markdown': markdown,
+        'type': _deckTypeToString(type),
+        'accepted': accepted,
+        'lastSyncAt': lastSyncAt.toUtc().millisecondsSinceEpoch,
+        'category': category,
+      };
 }
