4f46707f7f728006f179414007b33c797a622434
Artem Sheremet <dot.doom@gmail.com>
2018-07-01 18:31:03 +0200

    Make a few fields/classes private

--- lib/models/base/transaction.dart
+++ lib/models/base/transaction.dart
@@ -5,19 +5,19 @@ import 'package:firebase_database/firebase_database.dart';
 import 'model.dart';
 
 class Transaction {
-  final List<Model> toSave = List<Model>();
-  final List<Model> toDelete = List<Model>();
+  final List<Model> _toSave = List<Model>();
+  final List<Model> _toDelete = List<Model>();
 
-  void save(Model m) => toSave.add(m);
+  void save(Model m) => _toSave.add(m);
   void delete(Model m) {
     assert(m.key != null);
-    toDelete.add(m);
+    _toDelete.add(m);
   }
 
   Future<void> commit() {
     var root = FirebaseDatabase.instance.reference();
     var updates = Map<String, dynamic>();
-    toSave.forEach((m) {
+    _toSave.forEach((m) {
       if (m.key == null) {
         m.key = root.child(m.rootPath).push().key;
         updates.addAll(m.toMap(true));
@@ -25,7 +25,7 @@ class Transaction {
         updates.addAll(m.toMap(false));
       }
     });
-    toDelete.forEach((m) => updates['${m.rootPath}/${m.key}'] = null);
+    _toDelete.forEach((m) => updates['${m.rootPath}/${m.key}'] = null);
     return root.update(updates);
   }
 }
