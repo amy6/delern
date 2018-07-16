86a2f1df444c83c24424716bddf7309716ed03cd
Katarina Sheremet <kate.romanowskaya@gmail.com>
2018-07-16 11:05:30 +0200

    Notify user in DeckSettings on error deleting deck

--- lib/pages/deck_settings.dart
+++ lib/pages/deck_settings.dart
@@ -49,23 +49,31 @@ class _DeckSettingsPageState extends State<DeckSettingsPage> {
     if (_viewModelUpdates == null) {
       _viewModelUpdates = _viewModel.updates.listen((_) => setState(() {}));
     }
-    return new Scaffold(
-        appBar: new AppBar(
-          title: new Text(_viewModel.deck.name),
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(_viewModel.deck.name),
           actions: <Widget>[
-            new IconButton(
-                icon: new Icon(Icons.delete),
-                onPressed: () async {
-                  var locale = AppLocalizations.of(context);
-                  var deleteDeckDialog = await showSaveUpdatesDialog(
-                      context: context,
-                      changesQuestion: locale.deleteDeckQuestion,
-                      yesAnswer: locale.delete,
-                      noAnswer: locale.cancel);
-                  if (deleteDeckDialog && await _deleteDeck()) {
-                    Navigator.of(context).pop();
-                  }
-                })
+            Builder(
+              builder: (context) => IconButton(
+                  icon: Icon(Icons.delete),
+                  onPressed: () async {
+                    var locale = AppLocalizations.of(context);
+                    var deleteDeckDialog = await showSaveUpdatesDialog(
+                        context: context,
+                        changesQuestion: locale.deleteDeckQuestion,
+                        yesAnswer: locale.delete,
+                        noAnswer: locale.cancel);
+                    if (deleteDeckDialog) {
+                      try {
+                        await _viewModel.delete();
+                        Navigator.of(context).pop();
+                      } catch (e, stackTrace) {
+                        UserMessages.showError(
+                            Scaffold.of(context), e, stackTrace);
+                      }
+                    }
+                  }),
+            )
           ],
         ),
         body: _buildBody());
@@ -121,15 +129,5 @@ class _DeckSettingsPageState extends State<DeckSettingsPage> {
         ],
       ),
     );
-  }
-
-  Future<bool> _deleteDeck() async {
-    try {
-      await _viewModel.delete();
-      return true;
-    } catch (e, stacktrace) {
-      UserMessages.showError(Scaffold.of(context), e, stacktrace);
-      return false;
-    }
   }
 }
