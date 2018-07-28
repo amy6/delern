f2c5f8856389dbf3f7ace7064d9b0c8e86e4750c
Artem Sheremet <dot.doom@gmail.com>
2018-07-28 17:14:38 +0200

    Install keepSynced at least on Decks

--- lib/view_models/home_view_model.dart
+++ lib/view_models/home_view_model.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 import '../models/base/transaction.dart';
 import '../models/fcm.dart';
@@ -8,7 +9,19 @@ import '../models/user.dart';
 
 class HomeViewModel {
   static Future<void> userSignedIn(FirebaseUser firebaseUser, FCM fcm) {
-    // TODO(dotdoom): install keepSync etc.
+    // TODO(dotdoom): move keepSync into models, but need to:
+    // use User instead of FirebaseUser, for that need to
+    // move NavigationDrawer into Home, for that need to
+    // move Scaffold into Home => need AppBar in Home, for that need
+    // a generic SearchBar, or set Navigator in one place and AppBar in another.
+
+    FirebaseDatabase.instance
+        .reference()
+        .child('decks')
+        .child(firebaseUser.uid)
+        .keepSynced(true);
+    // TODO(dotdoom): learning/$uid; but that's too much!
+
     print('Registering for FCM as ${fcm.name} in ${fcm.language}');
     return (Transaction()
           ..save(User(
