0166a82a479959edddbdaec95ae12e200fa1efc5
Katarina Sheremet <kate.romanowskaya@gmail.com>
2018-10-15 18:54:35 +0200

    Show intro before sign in widget

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,12 +1,12 @@
 import 'dart:async';
 import 'dart:isolate';
 
-import 'package:intro_views_flutter/intro_views_flutter.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'flutter/localization.dart';
 import 'models/base/transaction.dart';
@@ -15,9 +15,26 @@ import 'views/decks_list/decks_list.dart';
 import 'views/helpers/sign_in_widget.dart';
 import 'views/onboarding/intro_view.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   static final _analyticsNavigatorObserver =
       FirebaseAnalyticsObserver(analytics: FirebaseAnalytics());
+
+  @override
+  AppState createState() => AppState();
+}
+
+class AppState extends State<App> {
+  static const String _introPrefKey = 'is-intro-shown';
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+  bool _isIntroShown;
+
+  @override
+  void initState() {
+    super.initState();
+    _prefs.then((pref) {
+      _isIntroShown = pref.getBool(_introPrefKey);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,30 +53,26 @@ class App extends StatelessWidget {
         Locale('en', 'US'),
         Locale('ru', 'RU'),
       ],
-      navigatorObservers: [_analyticsNavigatorObserver],
+      navigatorObservers: [App._analyticsNavigatorObserver],
       title: title,
       // SignInWidget must be above Navigator to provide CurrentUserWidget.of().
-      builder: (context, child) => SignInWidget(child: child),
+      builder: (context, child) {
+        if (_isIntroShown != null && _isIntroShown != false) {
+          return SignInWidget(child: child);
+        } else {
+          return IntroViewWidget(callback: () {
+            setState(() {
+              _prefs.then((pref) {
+                pref.setBool(_introPrefKey, true);
+                _isIntroShown = true;
+              });
+            });
+          });
+        }
+      },
       theme:
           ThemeData(primarySwatch: Colors.green, accentColor: Colors.redAccent),
-      home: Builder(
-        builder: (context) => IntroViewsFlutter(
-              IntroViewBuilder().introPages(context),
-              onTapDoneButton: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => DecksListPage(title: title),
-                  ),
-                );
-              },
-              showSkipButton: true,
-              pageButtonTextStyles: const TextStyle(
-                color: Colors.white,
-                fontSize: 18.0,
-              ),
-            ),
-      ),
+      home: DecksListPage(title: title),
     );
   }
 }
--- flutter/lib/views/onboarding/intro_view.dart
+++ flutter/lib/views/onboarding/intro_view.dart
@@ -1,39 +1,48 @@
 import 'package:intro_views_flutter/Models/page_view_model.dart';
+import 'package:intro_views_flutter/intro_views_flutter.dart';
+
 import 'package:flutter/material.dart';
 
 import '../../flutter/localization.dart';
 
-class IntroViewBuilder {
-  List<PageViewModel> introPages(BuildContext context) {
+class IntroViewWidget extends StatelessWidget {
+  static const double _imageHeight = 285.0;
+  static const double _imageWidth = 285.0;
+  static const _textStyle = TextStyle(color: Colors.white);
+  final Function callback;
+
+  const IntroViewWidget({@required this.callback}) : assert(callback != null);
+
+  List<PageViewModel> _introPages(BuildContext context) {
     var pages = [
       PageViewModel(
-          pageColor: const Color(0xFFFFB74D),
+          pageColor: const Color(0xFF3F51A5),
           body: Text(
             AppLocalizations.of(context).decksIntroDescription,
           ),
           title: Text(
             AppLocalizations.of(context).decksIntroTitle,
           ),
-          textStyle: const TextStyle(color: Colors.white),
+          textStyle: _textStyle,
           mainImage: Image.asset(
             'images/deck_creation.png',
-            height: 285.0,
-            width: 285.0,
+            height: _imageHeight,
+            width: _imageWidth,
             alignment: Alignment.center,
           )),
       PageViewModel(
-        pageColor: const Color(0xFF8BC34A),
+        pageColor: const Color(0xFFFFB74D),
         body: Text(
           AppLocalizations.of(context).learnIntroDescription,
         ),
         title: Text(AppLocalizations.of(context).learnIntroTitle),
         mainImage: Image.asset(
           'images/child_learning.png',
-          height: 285.0,
-          width: 285.0,
+          height: _imageHeight,
+          width: _imageWidth,
           alignment: Alignment.center,
         ),
-        textStyle: const TextStyle(color: Colors.white),
+        textStyle: _textStyle,
       ),
       PageViewModel(
         pageColor: const Color(0xFF607D8B),
@@ -43,13 +52,24 @@ class IntroViewBuilder {
         title: Text(AppLocalizations.of(context).shareIntroTitle),
         mainImage: Image.asset(
           'images/card_sharing.png',
-          height: 285.0,
-          width: 285.0,
+          height: _imageHeight,
+          width: _imageWidth,
           alignment: Alignment.center,
         ),
-        textStyle: const TextStyle(color: Colors.white),
+        textStyle: _textStyle,
       ),
     ];
     return pages;
   }
+
+  @override
+  Widget build(BuildContext context) => IntroViewsFlutter(
+        _introPages(context),
+        onTapDoneButton: callback,
+        showSkipButton: true,
+        pageButtonTextStyles: const TextStyle(
+          color: Colors.white,
+          fontSize: 18.0,
+        ),
+      );
 }
