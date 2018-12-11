7e8a2304c3164503848bf737c29ea776b05884a5
Katarina Sheremet <kate.romanowskaya@gmail.com>
2018-12-11 15:12:11 +0100

    Place create deck dialog in landscape #862

--- flutter/lib/views/decks_list/create_deck.dart
+++ flutter/lib/views/decks_list/create_deck.dart
@@ -49,38 +49,136 @@ class _CreateDeckDialogState extends State<_CreateDeckDialog> {
   final TextEditingController _textController = TextEditingController();
 
   @override
-  Widget build(BuildContext context) => AlertDialog(
+  Widget build(BuildContext context) {
+    final addDeckButton = FlatButton(
+        child: Text(
+          AppLocalizations.of(context).add.toUpperCase(),
+          style: TextStyle(
+              color: _textController.text.isEmpty
+                  ? Theme.of(context).disabledColor
+                  : Theme.of(context).accentColor),
+        ),
+        onPressed: _textController.text.isEmpty
+            ? null
+            : () {
+                Navigator.of(context).pop(Deck(
+                    uid: CurrentUserWidget.of(context).user.uid,
+                    name: _textController.text));
+              });
+
+    final cancelButton = FlatButton(
+        onPressed: () {
+          Navigator.of(context).pop(null);
+        },
+        child: Text(
+          MaterialLocalizations.of(context).cancelButtonLabel.toUpperCase(),
+          style: TextStyle(color: Theme.of(context).accentColor),
+        ));
+
+    final deckNameTextField = TextField(
+      autofocus: true,
+      controller: _textController,
+      onChanged: (text) {
+        setState(() {});
+      },
+      style: AppStyles.primaryText,
+    );
+
+    if (MediaQuery.of(context).orientation == Orientation.portrait) {
+      return AlertDialog(
         title: Text(
           AppLocalizations.of(context).deck,
           style: const TextStyle(fontWeight: FontWeight.w600),
         ),
         content: SingleChildScrollView(
-          child: TextField(
-            autofocus: true,
-            controller: _textController,
-            onChanged: (text) {
-              setState(() {});
-            },
-            style: AppStyles.primaryText,
-          ),
+          child: deckNameTextField,
         ),
         actions: <Widget>[
-          FlatButton(
-              onPressed: () {
-                Navigator.of(context).pop(null);
-              },
-              child: Text(MaterialLocalizations.of(context)
-                  .cancelButtonLabel
-                  .toUpperCase())),
-          FlatButton(
-              child: Text(AppLocalizations.of(context).add.toUpperCase()),
-              onPressed: _textController.text.isEmpty
-                  ? null
-                  : () {
-                      Navigator.of(context).pop(Deck(
-                          uid: CurrentUserWidget.of(context).user.uid,
-                          name: _textController.text));
-                    }),
+          cancelButton,
+          addDeckButton,
         ],
       );
+    } else {
+      return HorizontalDialog(
+        child: SingleChildScrollView(
+          child: Container(
+              width: MediaQuery.of(context).size.width,
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: MediaQuery.of(context).size.width * 0.05),
+                child: Row(
+                  children: <Widget>[
+                    Flexible(child: deckNameTextField),
+                    Column(
+                      children: <Widget>[
+                        addDeckButton,
+                        cancelButton,
+                      ],
+                    )
+                  ],
+                ),
+              )),
+        ),
+      );
+    }
+  }
+}
+
+class HorizontalDialog extends StatelessWidget {
+  const HorizontalDialog({
+    Key key,
+    this.child,
+    this.insetAnimationDuration = const Duration(milliseconds: 100),
+    this.insetAnimationCurve = Curves.decelerate,
+    this.shape,
+  }) : super(key: key);
+
+  final Widget child;
+
+  /// The duration of the animation to show when the system keyboard intrudes
+  /// into the space that the dialog is placed in.
+  ///
+  /// Defaults to 100 milliseconds.
+  final Duration insetAnimationDuration;
+
+  /// The curve to use for the animation shown when the system keyboard intrudes
+  /// into the space that the dialog is placed in.
+  ///
+  /// Defaults to [Curves.fastOutSlowIn].
+  final Curve insetAnimationCurve;
+
+  /// {@template flutter.material.dialog.shape}
+  /// The shape of this dialog's border.
+  ///
+  /// Defines the dialog's [Material.shape].
+  ///
+  /// The default shape is a [RoundedRectangleBorder] with a radius of 2.0.
+  /// {@endtemplate}
+  final ShapeBorder shape;
+
+  Color _getColor(BuildContext context) =>
+      Theme.of(context).dialogBackgroundColor;
+
+  static const RoundedRectangleBorder _defaultDialogShape =
+      RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(2.0)));
+
+  @override
+  Widget build(BuildContext context) {
+    final dialogTheme = DialogTheme.of(context);
+    return AnimatedPadding(
+      padding: MediaQuery.of(context).viewInsets,
+      duration: insetAnimationDuration,
+      curve: insetAnimationCurve,
+      child: Center(
+        child: Material(
+          elevation: 24.0,
+          color: _getColor(context),
+          type: MaterialType.card,
+          child: child,
+          shape: shape ?? dialogTheme.shape ?? _defaultDialogShape,
+        ),
+      ),
+    );
+  }
 }
