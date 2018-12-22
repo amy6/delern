102e60b2a98eceea93dd45b27e031193968153b3
Artem Sheremet <dot.doom@gmail.com>
2018-12-22 11:12:09 +0100

    Add annotations to Enum methods

--- flutter/lib/models/base/enum.dart
+++ flutter/lib/models/base/enum.dart
@@ -1,6 +1,11 @@
+/// Helper generic methods for enums.
 class Enum {
+  /// Returns the name of the enum [value], non-qualified (i.e. the last part).
   static String asString(value) => value?.toString()?.split('.')?.last;
 
+  /// Finds non-qualified enum value that matches the [value] string, or returns
+  /// null. Comparison is case sensitive. [enumValues] has to list all possible
+  /// enum values, which is usually available as <EnumType>.values.
   static T fromString<T>(String value, List<T> enumValues) =>
       enumValues.firstWhere((enumValue) => asString(enumValue) == value,
           orElse: () => null);
