f65b6a09429bd87980a9e56138e99cbd15763bfb
Artem Sheremet <dot.doom@gmail.com>
2018-12-23 11:59:01 +0100

    Enable Firebase DB log in debug builds (Android)
    We had this before in Android version:
    https://github.com/dasfoo/delern/blob/android/app/src/main/java/org/dasfoo/delern/DelernApplication.java#L80-L82

--- flutter/android/app/src/main/java/org/dasfoo/delern/MainActivity.java
+++ flutter/android/app/src/main/java/org/dasfoo/delern/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.provider.Settings;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Logger;
 
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
@@ -16,8 +18,14 @@ public class MainActivity extends FlutterActivity {
     String testLabSetting = Settings.System.getString(getContentResolver(), "firebase.test.lab");
     if (!"true".equals(testLabSetting)) {
       // Re-enable Analytics when not in Firebase Test Lab. See AndroidManifest.xml for details.
-      // Documentation on detecting Firebase Test Lab: https://goo.gl/JeHQuW
+      // Documentation on detecting Firebase Test Lab:
+      // https://firebase.google.com/docs/test-lab/android/android-studio
       FirebaseAnalytics.getInstance(this).setAnalyticsCollectionEnabled(true);
+    }
+
+    if (BuildConfig.DEBUG) {
+      // TODO(dotdoom): this should be placed in main.dart once it's available in Flutter.
+      FirebaseDatabase.getInstance().setLogLevel(Logger.Level.DEBUG);
     }
 
     GeneratedPluginRegistrant.registerWith(this);
