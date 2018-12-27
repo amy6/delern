977d972965c857278d11c89848bc7421db599ad1
Artem Sheremet <dot.doom@gmail.com>
2018-12-27 08:26:50 +0100

    Migrate Firebase functions to Typescript

---
+++ firebase/functions/src/index.ts
@@ -0,0 +1,262 @@
+import * as cors from 'cors';
+import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions';
+import * as nodemailer from 'nodemailer';
+
+admin.initializeApp();
+
+let mailTransport = null;
+if ('gmail' in functions.config()) {
+    // TODO(dotdoom): change this to something like:
+    //   nodemailer.createTransport(functions.config().email).
+    const gmailConfig = functions.config().gmail;
+    mailTransport = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: gmailConfig.email,
+            pass: gmailConfig.password,
+        },
+    });
+}
+
+const delern = {
+    createScheduledCardObject: () => {
+        return {
+            level: 'L0',
+            // TODO(dotdoom): figure out better repeatAt
+            repeatAt: 0,
+        };
+    },
+    createMissingScheduledCards: async (uid: string, deckKey: string) => {
+        const scheduledCards = (await admin.database().ref('learning')
+            .child(uid).child(deckKey).once('value')).val() || {};
+        const cards = (await admin.database().ref('cards').child(deckKey)
+            .once('value')).val() || {};
+
+        const scheduledCardsUpdates = {};
+        for (const cardKey in cards) {
+            if (!(cardKey in scheduledCards)) {
+                scheduledCardsUpdates[cardKey] =
+                    delern.createScheduledCardObject();
+            }
+        }
+
+        // TODO(dotdoom): we already do this on app side, not necessary.
+        for (const cardKey in scheduledCards) {
+            if (!(cardKey in cards)) {
+                scheduledCardsUpdates[cardKey] = null;
+            }
+        }
+
+        if (Object.keys(scheduledCardsUpdates).length !== 0) {
+            console.error(Error('Database denormalized in deck ' + deckKey +
+                ' for user ' + uid + ', fixing (see below for details)'));
+            console.log(scheduledCardsUpdates);
+        }
+
+        await admin.database().ref('learning').child(uid).child(deckKey)
+            .update(scheduledCards);
+    },
+    setScheduledCardForAllUsers: async (deckKey: string, cardKey: string,
+        skipUid: string, scheduledCard: any) => {
+        const deckAccesses = (await admin.database()
+            .ref('deck_access').child(deckKey).once('value')).val();
+
+        const learningUpdate = {};
+        for (const sharedWithUid in deckAccesses) {
+            if (sharedWithUid !== skipUid) {
+                learningUpdate[[sharedWithUid, deckKey, cardKey].join('/')] =
+                    scheduledCard;
+            }
+        }
+        await admin.database().ref('learning').update(learningUpdate);
+    },
+    forEachUser: null,
+};
+
+export const userLookup = functions.https.onRequest((req, res) =>
+    // https://firebase.google.com/docs/functions/http-events
+    cors({ origin: true })(req, res, async () => {
+        // TODO(dotdoom): check auth, e.g.:
+        // https://github.com/firebase/functions-samples/tree/master/authorized-https-endpoint
+
+        if (!req.query.q) {
+            res.status(400).end();
+            return;
+        }
+
+        try {
+            res.send(
+                (await admin.auth().getUserByEmail(req.query.q)).uid);
+        } catch (error) {
+            // TODO(dotdoom): getUserByPhoneNumber.
+            res.status(404).end();
+        }
+    }));
+
+export const deckShared = functions.database
+    .ref('/deck_access/{deckKey}/{sharedWithUid}')
+    .onCreate(async (data, context) => {
+        if (data.val().access === 'owner') {
+            console.log('Deck is being created (not shared), skipping');
+            return;
+        }
+
+        const deckKey = context.params.deckKey;
+        const sharedWithUser =
+            await admin.auth().getUser(context.params.sharedWithUid);
+
+        const scheduledCards = {};
+        const cards = (await admin.database()
+            .ref('cards').child(deckKey).once('value')).val();
+        for (const cardKey in cards) {
+            scheduledCards[cardKey] =
+                delern.createScheduledCardObject();
+        }
+        await admin.database()
+            .ref('learning').child(sharedWithUser.uid).child(deckKey)
+            .set(scheduledCards);
+
+        if (context.authType !== 'USER') {
+            // If the deck is shared by admin, we do not send notifications.
+            return;
+        }
+
+        const numberOfCards = Object.keys(scheduledCards).length;
+        const actorUser = await admin.auth().getUser(context.auth.uid);
+        const deckName = (await admin.database()
+            .ref('decks').child(sharedWithUser.uid).child(deckKey).child('name')
+            .once('value')).val();
+        const mailOptions = {
+            // Either "from" or "reply-to" will work with most servers/clients.
+            from: {
+                name: actorUser.displayName + ' via Delern',
+                address: actorUser.email,
+            },
+            replyTo: actorUser.email,
+            to: sharedWithUser.email,
+            subject: actorUser.displayName + ' shared a Delern deck with you',
+            text: 'Hello! ' + actorUser.displayName + ' has shared a Delern ' +
+                'deck "' + deckName + '" (' + numberOfCards + ' cards) ' +
+                'with you! Go to the Delern app on your device to check it out',
+        };
+        console.log('Sending notification email', mailOptions);
+        try {
+            await mailTransport.sendMail(mailOptions);
+        } catch (e) {
+            console.error('Cannot send email', e);
+        }
+
+        const fcmSnapshot = admin.database().ref('fcm')
+            .child(sharedWithUser.uid);
+        const fcmEntries = (await fcmSnapshot.once('value')).val() || {};
+        const payload = {
+            notification: {
+                title: actorUser.displayName + ' shared a deck with you',
+                body: actorUser.displayName + ' shared their deck "' +
+                    deckName + '" (' + numberOfCards + ' cards) with you',
+                icon: actorUser.photoURL || '',
+            },
+        };
+
+        for (const fcmId in fcmEntries) {
+            console.log('Notifying user ' + sharedWithUser.uid + ' on ' +
+                fcmEntries[fcmId].name + ' about user ' + actorUser.uid +
+                ' sharing a deck ' + deckName + ' (' + numberOfCards +
+                ' cards)');
+        }
+
+        const tokensToRemoveOperations = [];
+        for (const result of (await admin.messaging().sendToDevice(
+            Object.keys(fcmEntries), payload)).results) {
+            const error = result.error;
+            if (error) {
+                // Cleanup the tokens which are not registered anymore.
+                if (error.code === 'messaging/invalid-registration-token' ||
+                    error.code ===
+                    'messaging/registration-token-not-registered') {
+                    console.warn('Removing a token because of', error.code);
+                    tokensToRemoveOperations.push(
+                        fcmSnapshot.ref.child(result.canonicalRegistrationToken)
+                            .remove());
+                } else {
+                    console.error('Failed to send a notification to',
+                        sharedWithUser.uid, ':', error.code);
+                }
+            }
+        }
+        await Promise.all(tokensToRemoveOperations);
+    });
+
+export const deckUnShared = functions.database
+    .ref('/deck_access/{deckKey}/{uid}')
+    .onDelete((data, context) => {
+        const deckKey = context.params.deckKey;
+        const uid = context.params.uid;
+
+        return admin.database().ref('/').update({
+            [['learning', uid, deckKey].join('/')]: null,
+            [['views', uid, deckKey].join('/')]: null,
+            [['decks', uid, deckKey].join('/')]: null,
+        });
+    });
+
+export const cardAdded = functions.database.ref('/cards/{deckKey}/{cardKey}')
+    .onCreate((data, context) =>
+        delern.setScheduledCardForAllUsers(context.params.deckKey,
+            // Don't update for the user creating this card - it is done by app.
+            context.params.cardKey, context.auth.uid,
+            delern.createScheduledCardObject()));
+
+// TODO(dotdoom): deleting deck with too many cards may be bad. Debounce?
+export const cardDeleted = functions.database.ref('/cards/{deckKey}/{cardKey}')
+    .onDelete((data, context) =>
+        delern.setScheduledCardForAllUsers(context.params.deckKey,
+            context.params.cardKey, context.auth.uid, null));
+
+delern.forEachUser = async (batchSize: number,
+    callback: (user: admin.auth.UserRecord) => Promise<void>,
+    pageToken?: string) => {
+    const listUsersResult = await admin.auth().listUsers(batchSize,
+        pageToken);
+
+    await Promise.all(listUsersResult.users.map(callback));
+
+    if (listUsersResult.pageToken) {
+        await delern.forEachUser(batchSize, callback,
+            listUsersResult.pageToken);
+    }
+};
+
+export const databaseMaintenance = functions.https
+    .onRequest(async (req, res) => {
+        const deckAccesses = (await admin.database()
+            .ref('deck_access').once('value')).val();
+        const uidCache = {};
+        const userInformationUpdates = {};
+        const missingCardsOperations = [];
+        for (const deckKey in deckAccesses) {
+            const deckAccess = deckAccesses[deckKey];
+
+            for (const uid in deckAccess) {
+                if (!(uid in uidCache)) {
+                    uidCache[uid] = await admin.auth().getUser(uid);
+                }
+                const user = uidCache[uid];
+                userInformationUpdates[
+                    [deckKey, uid, 'displayName'].join('/')] =
+                    user.displayName || null;
+                userInformationUpdates[
+                    [deckKey, uid, 'photoUrl'].join('/')] =
+                    user.photoUrl || null;
+
+                missingCardsOperations.push(
+                    delern.createMissingScheduledCards(uid, deckKey));
+            }
+        }
+        await Promise.all(missingCardsOperations);
+        await admin.database().ref('deck_access')
+            .update(userInformationUpdates);
+
+        res.end();
+    });
