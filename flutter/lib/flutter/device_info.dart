64dacb03a85960d3081cd4bf817320aed575ac90
Artem Sheremet <dot.doom@gmail.com>
2019-01-06 20:44:06 +0100

    Unify code for error reporting and contact us

--- flutter/lib/flutter/device_info.dart
+++ flutter/lib/flutter/device_info.dart
@@ -4,33 +4,54 @@ import 'dart:io';
 import 'package:delern_flutter/remote/error_reporting.dart';
 import 'package:device_info/device_info.dart';
 import 'package:flutter/material.dart';
-import 'package:meta/meta.dart';
 
+@immutable
 class DeviceInfo {
   final String userFriendlyName;
-  final int sdk;
+  final Map<String, dynamic> info;
 
   static DeviceInfo _instance;
 
-  DeviceInfo._({@required this.userFriendlyName, @required this.sdk});
+  DeviceInfo._(
+      {@required this.userFriendlyName, @required Map<String, dynamic> info})
+      : assert(userFriendlyName != null),
+        assert(info != null),
+        this.info = Map.unmodifiable(info);
 
   static Future<DeviceInfo> getDeviceInfo() async {
     if (_instance == null) {
-      _instance = DeviceInfo._(
-          userFriendlyName: Platform.operatingSystem,
-          sdk: Platform.numberOfProcessors);
+      // Set default instance with basic info if everything else fails.
+      _instance =
+          DeviceInfo._(userFriendlyName: Platform.operatingSystem, info: {
+        // Ex.: android Linux 3.2.1 SMP etc.
+        'OS': '${Platform.operatingSystem} ${Platform.operatingSystemVersion}',
+        // Ex.: 2.1.0-dev-flutter etc.
+        'Dart': Platform.version,
+      });
 
       try {
-        var deviceInfo = DeviceInfoPlugin();
+        final deviceInfo = DeviceInfoPlugin();
         if (Platform.isAndroid) {
-          var info = await deviceInfo.androidInfo;
-          _instance = DeviceInfo._(
-              userFriendlyName: '${info.manufacturer} ${info.model}',
-              sdk: info.version.sdkInt);
+          final androidInfo = await deviceInfo.androidInfo;
+          final userFriendlyName =
+              '${androidInfo.manufacturer} ${androidInfo.model}';
+          // https://developer.android.com/reference/android/os/Build.VERSION
+          _instance = DeviceInfo._(userFriendlyName: userFriendlyName, info: {
+            'Device': userFriendlyName,
+            'Android version': androidInfo.version.release,
+            'Android Security Patch level': androidInfo.version.securityPatch,
+            'SDK': androidInfo.version.sdkInt,
+          });
         } else if (Platform.isIOS) {
-          var info = await deviceInfo.iosInfo;
+          final iosInfo = await deviceInfo.iosInfo;
+          // http://pubs.opengroup.org/onlinepubs/7908799/xsh/sysutsname.h.html
           _instance =
-              DeviceInfo._(userFriendlyName: info.model.toString(), sdk: 0);
+              DeviceInfo._(userFriendlyName: iosInfo.utsname.machine, info: {
+            'Device': iosInfo.model,
+            'iOS version': '${iosInfo.systemName} ${iosInfo.systemVersion}',
+            'Version level of the release': iosInfo.utsname.version,
+            'Hardware Type': iosInfo.utsname.machine,
+          });
         }
       } catch (e, stackTrace) {
         // Can't report directly because ErrorReporting uses this method.
